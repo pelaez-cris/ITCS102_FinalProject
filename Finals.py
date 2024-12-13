c8d45a64402c3c9117eb371e7eb7c76b712b1b17
CrisLaurencePelaez <pelaezcrislaurence@gmail.com>
2024-12-13 22:45:58 +0800

    pogi c derick

--- Finals.py
+++ Finals.py
@@ -1,6 +1,19 @@
 import os
-def mainmenu():
-    print(f"\nCris Laurence Pelaez")
-    print(f"BSIT-1C")
-    print(f"Code Compilation")
-mainmenu()
+
+# tuloy = True
+# while tuloy == True:
+print(f"\nCris Laurence Pelaez")
+print(f"BSIT-1C")
+print(f"Code Compilation")
+print(f"\nActivities---> 1 ")
+print(f"Code Challenges---> 2 ")
+print(f"Exit Program---> 3 ")
+menmen = int(input("\nChoose Number what You want to do---> "))
+# if menmen == 1:
+       
+
+
+
+
+
+
