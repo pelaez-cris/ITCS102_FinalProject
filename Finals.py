73b7a166b8240eed34ebfa2d4f084f2a8abafce1
Cris Laurence P. Pelaez <pelaezcrislaurence@gmail.com>
2024-12-13 14:47:38 +0800

    Update Finals.py

--- Finals.py
+++ Finals.py
@@ -1,8 +1,12 @@
 import os
-os.system("cls")
 def body():
     print(f"\nWelcome to code compilation of Cris Laurence Pelaez")
     print(f"BSIT-1C")
-   
+    print(f"\nChoose what action you want to do:")
+    print(f"\n1. Open Activity Folder")  
+    print(f"2. Open Code Challenge Folder")
+    print(f"3. Exit Program")
 
-     
+body()
+# tuloy = True
+#     while tuloy == True: cls
