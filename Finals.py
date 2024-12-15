06a7df7bbf0c2b73e49a50eb50f1c28c28901421
CrisLaurencePelaez <pelaezcrislaurence@gmail.com>
2024-12-15 23:58:02 +0800

    gg

--- Finals.py
+++ Finals.py
@@ -1,33 +1,11 @@
 import os
-<<<<<<< HEAD
-
-# tuloy = True
-# while tuloy == True:
-print(f"\nCris Laurence Pelaez")
-print(f"BSIT-1C")
-print(f"Code Compilation")
-print(f"\nActivities---> 1 ")
-print(f"Code Challenges---> 2 ")
-print(f"Exit Program---> 3 ")
-menmen = int(input("\nChoose Number what You want to do---> "))
-# if menmen == 1:
-       
-
-
-
-
-
-
-=======
 def body():
     print(f"\nWelcome to code compilation of Cris Laurence Pelaez")
-    print(f"BSIT-1C")
+    print(f"Bachelor Of Science In Infomation Technology-1C")
     print(f"\nChoose what action you want to do:")
     print(f"\n1. Open Activity Folder")  
     print(f"2. Open Code Challenge Folder")
     print(f"3. Exit Program")
-
 body()
 # tuloy = True
 #     while tuloy == True: cls
->>>>>>> 73b7a166b8240eed34ebfa2d4f084f2a8abafce1
