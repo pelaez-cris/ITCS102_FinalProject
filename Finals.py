e446b7a94946ca30f96c84c6ab8f603ada939a8e
CrisLaurencePelaez <pelaezcrislaurence@gmail.com>
2024-12-16 00:09:10 +0800

    ggg

--- Finals.py
+++ Finals.py
@@ -2,10 +2,9 @@ import os
 def body():
     print(f"\nWelcome to code compilation of Cris Laurence Pelaez")
     print(f"Bachelor Of Science In Infomation Technology-1C")
-    print(f"\nChoose what action you want to do:")
+    menu = input(f"\nChoose what action you want to do:")
     print(f"\n1. Open Activity Folder")  
     print(f"2. Open Code Challenge Folder")
     print(f"3. Exit Program")
 body()
-# tuloy = True
-#     while tuloy == True: cls
+
