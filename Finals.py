bfd5e04151b54c3dee8be775cfe8b69ad8f3f8fc
CrisLaurencePelaez <pelaezcrislaurence@gmail.com>
2024-12-16 02:36:37 +0800

    Update Finals.py

--- Finals.py
+++ Finals.py
@@ -1,10 +1,33 @@
 import os
+user == True:
+
 def body():
+        print(f"\nWelcome to code compilation of Cris Laurence Pelaez")
+        print(f"Bachelor Of Science In Infomation Technology-1C")
+        print(f"\nPlease Select an Option:   ")
+        print(f"\n1. Open Activity Folder")  
+        print(f"2. Open Code Challenge Folder")
+        print(f"3. Exit Program")
+body()
+menu = input(f"\nChoose what action you want to do:  ")
+while body:
+    if menu == "1":
+        menu == act_folder()
+    elif menu == "2":
+        menu == cc_folder()
+    elif menu == "3":
+        print(f"\nProgram terminated, Thank you for checking my Program.\n")
+        break
+    else:
+        print("Invalid Choice, Please try again.")
+body()
+
+
+
+def act_folder():
     print(f"\nWelcome to code compilation of Cris Laurence Pelaez")
     print(f"Bachelor Of Science In Infomation Technology-1C")
-    menu = input(f"\nChoose what action you want to do:")
-    print(f"\n1. Open Activity Folder")  
-    print(f"2. Open Code Challenge Folder")
-    print(f"3. Exit Program")
-body()
+def cc_folder():
+    print(f"\nWelcome to code compilation of Cris Laurence Pelaez")
+    print(f"Bachelor Of Science In Infomation Technology-1C")
 
